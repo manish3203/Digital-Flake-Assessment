69f24ef60fbeebf45316b5ece4b851b292553e93
manish3203 <manishchavan3203@gmail.com>
2024-04-15 14:39:36 +0530

    main

--- digital_flake_assess/lib/main.dart
+++ digital_flake_assess/lib/main.dart
@@ -1,3 +1,4 @@
+import "package:digital_flake_assess/Screens/Authentication/login_page.dart";
 import "package:flutter/material.dart";
 
 void main() {
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
